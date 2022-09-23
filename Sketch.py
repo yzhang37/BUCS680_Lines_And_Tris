d9cb1500072972c1b74fcf2160e51f0b0e825b91
青椒 <10142130151_ecnu@outlook.com>
2022-09-23 14:54:00 -0400

    y-first iteration changes to x-first iteration

--- Sketch.py
+++ Sketch.py
@@ -157,6 +157,8 @@ class Sketch(CanvasBase):
             if self.debug > 0:
                 print("draw a triangle {} -> {} -> {}".format(self.points_r[-3], self.points_r[-2], self.points_r[-1]))
             self.drawPoint(self.buff, self.points_r[-1])
+            self.drawTriangle(self.buff, self.points_r[-3], self.points_r[-2], self.points_r[-1],
+                              self.doSmooth, self.doAA, self.doAAlevel, self.doTexture)
             self.points_r.clear()
 
     def Interrupt_Keyboard(self, keycode: int):
@@ -391,55 +393,66 @@ class Sketch(CanvasBase):
         #   3. TODO: You should be able to support both flat shading and smooth shading, which is controlled by doSmooth
         #   4. TODO: For texture-mapped fill of triangles, it should be controlled by doTexture flag.
 
-        # Sort the three points by the y-coordinate from smallest to largest
+        # Sort the three points by the x-value from smallest to largest
         points = [p1, p2, p3]
-        points.sort(key=lambda p: p.coords[1])
-        # Draw the triangle as two parts, top and bottom.
+        points.sort(key=lambda p: p.coords[0])
+        # Draw the triangle as two parts, left and right.
         first_point = last_point = None
         middle_point_1 = middle_point_2 = None
 
-        # If the y-value of the 1st point and the 2nd point are not the same
-        if points[0].coords[1] < points[1].coords[1]:
+        # If the x-value of the 1st point and the 2nd point are not the same
+        if points[0].coords[0] < points[1].coords[0]:
             first_point = points[0]
-            # If the y-value of the 2nd point and the 3rd point are not the same
-            if points[1].coords[1] < points[2].coords[1]:
+            # If the x-value of the 2nd point and the 3rd point are not the same
+            if points[1].coords[0] < points[2].coords[0]:
                 last_point = points[2]
                 # Select the point as the middle point
                 middle_point_1 = points[1]
                 # Another point needs to be interpolated,
                 # calculating the x-coordinate and the value of rgb
-                new_y = middle_point_1.coords[1]
+                new_x = middle_point_1.coords[0]
                 new_t = (middle_point_1.coords[1] -
                          first_point.coords[1]) / (last_point.coords[1] - first_point.coords[1])
-                new_x = first_point.coords[0] * (1 - new_t) + last_point.coords[0] * new_t
+                new_y = first_point.coords[1] * (1 - new_t) + last_point.coords[1] * new_t
                 new_r = first_point.color.r * (1 - new_t) + last_point.color.r * new_t
                 new_g = first_point.color.g * (1 - new_t) + last_point.color.g * new_t
                 new_b = first_point.color.b * (1 - new_t) + last_point.color.b * new_t
                 middle_point_2 = Point((new_x, new_y), ColorType(new_r, new_g, new_b))
             else:
-                # If y-value of the second point and the third point are same
+                # If x-value of the second point and the third point are same
                 middle_point_1 = points[1]
                 middle_point_2 = points[2]
 
-            # Middle two points should be sorted from smallest to largest
-            if middle_point_1.coords[0] > middle_point_2.coords[0]:
+            # Sort middle points' y-value from smallest to largest
+            if middle_point_1.coords[1] > middle_point_2.coords[1]:
                 middle_point_1, middle_point_2 = middle_point_2, middle_point_1
         else:
             middle_point_1 = points[0]
             middle_point_2 = points[1]
-            # If the second point and the third point has different y-value
-            if points[1].coords[1] < points[2].coords[1]:
+            # If the second point and the third point has different x-value
+            if points[1].coords[0] < points[2].coords[0]:
                 last_point = points[2]
             else:
                 # Show that the three points are on the same horizontal line and
                 # therefore the triangle is degenerate into a horizontal straight line.
-                # Sort the three points by x coordinate from smallest to largest
-                points.sort(key=lambda p: p.coords[0])
+                # Sort the three points by y coordinate from smallest to largest
+                points.sort(key=lambda p: p.coords[1])
                 middle_point_1 = points[0]
                 middle_point_2 = points[2]
 
-        
+        # TODO: for testing only
+        if self.debug:
+            print("Triangle details:")
+            print(f"first_point: {first_point.coords if first_point else None}")
+            print(f"middle_points: {middle_point_1.coords}, {middle_point_2.coords}")
+            print(f"last_point: {last_point.coords if last_point else None}")
 
+        if first_point is not None:
+            # TODO: Draw the top part
+            pass
+        if last_point is not None:
+            # TODO: Draw the bottom part
+            pass
         return
 
     # test for lines lines in all directions
@@ -604,5 +617,5 @@ if __name__ == "__main__":
         stats.print_stats()
 
 
-    main()
-    # codingDebug()
+    # main()
+    codingDebug()
