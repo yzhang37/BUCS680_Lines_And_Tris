044c189b9da43864f2532134188aad0683ec41d0
青椒 <10142130151_ecnu@outlook.com>
2022-09-25 22:32:04 -0400

    draw line when double clicking right buttons.

--- Sketch.py
+++ Sketch.py
@@ -161,7 +161,8 @@ class Sketch(CanvasBase):
         elif len(self.points_r) % 3 == 2:
             if self.debug > 0:
                 print("draw a line from ", self.points_r[-1], " -> ", self.points_r[-2])
-            self.drawPoint(self.buff, self.points_r[-1])
+            self.drawLine(self.buff, self.points_r[-1], self.points_r[-2],
+                          self.doSmooth, self.doAA, self.doAAlevel)
         elif len(self.points_r) % 3 == 0 and len(self.points_r) > 0:
             if self.debug > 0:
                 print("draw a triangle {} -> {} -> {}".format(self.points_r[-3], self.points_r[-2], self.points_r[-1]))
